4540c5f405d0f1f2ca680d5bc2b1082bc4bc0478
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2026-05-08 09:18:37 -0300

    up

--- semana2.js
+++ semana2.js
@@ -1,1 +1,23 @@
+    
+
+
+    const adicionar = (lista) => {
+        lista.push(frutas)
+        return lista
+
+    }
+let frutas = ["Uva", "Limão"]
+frutas = adicionar(frutas, "Abacaxi")
+console.log(frutas)
+
+const remover = (lista) => {
+        lista.shift(frutas)
+        return lista
+
+    }
+let frutas = ["Uva", "Limão", "Abacaxi"]
+frutas = remover(frutas, "Uva")
+console.log(frutas)
+shift()
+
 
